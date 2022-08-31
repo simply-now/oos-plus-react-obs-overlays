92035d61349569be945917d3c5429cb48290e1af
simply-now <95588659+simply-now@users.noreply.github.com>
2022-08-31 06:08:20 -0700

    implemented fix to package dependacies that should work fingers crossed. Also implemented flip button. Next need to adjust style for buttons to make them easier to use. Then keyboard shortcuts modal rules view and search modal view

--- src/app/appFeatures/ScryfallCardForm/index.tsx
+++ src/app/appFeatures/ScryfallCardForm/index.tsx
@@ -267,6 +267,13 @@ export function ScryfallCardForm() {
     inputElement.select();
   };
 
+  const flipCard = (elementId: string) => {
+    const elementToFlip = document.getElementById(elementId);
+    if (elementToFlip) {
+      elementToFlip.classList.toggle('active');
+    }
+  };
+
   const onClick: React.MouseEventHandler<HTMLElement> = e => {
     if (e.currentTarget.style.transform !== 'rotate(' + 90 + 'deg)') {
       e.currentTarget.style.transform = 'rotate(' + 90 + 'deg)';
@@ -386,11 +393,12 @@ export function ScryfallCardForm() {
                 ) : card.card_faces?.length === 2 ? (
                   <CardImageContainer
                     onClick={rotateElement}
-                    className="cardContainer inactive"
+                    className="cardContainer"
                   >
                     <CardImages
                       key={card.name + 'cardImagesBackFront'}
                       className="card"
+                      id={'cardImagesBackFront' + card.name}
                     >
                       <CardImage
                         key={card.name + 'cardImage-front'}
@@ -418,6 +426,29 @@ export function ScryfallCardForm() {
                   <ErrorText>{repoErrorText(error)}</ErrorText>
                 ) : null}
               </CardImageRoot>
+              {card.card_faces?.length === 2 ? (
+                <CardBtnlist className="btn-list">
+                  <CardBtn
+                    onClick={function () {
+                      flipCard('cardImagesBackFront' + card.name);
+                    }}
+                    className="btn"
+                  >
+                    <CardBtnTitle>Flip</CardBtnTitle>
+                    <CardBtnSVG
+                      fill="currentColor"
+                      height="24"
+                      viewBox="0 0 512 512"
+                      width="24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M449.9 39.96l-48.5 48.53C362.5 53.19 311.4 32 256 32C161.5 32 78.59 92.34 49.58 182.2c-5.438 16.81 3.797 34.88 20.61 40.28c16.97 5.5 34.86-3.812 40.3-20.59C130.9 138.5 189.4 96 256 96c37.96 0 73 14.18 100.2 37.8L311.1 178C295.1 194.8 306.8 223.4 330.4 224h146.9C487.7 223.7 496 215.3 496 204.9V59.04C496 34.99 466.9 22.95 449.9 39.96zM441.8 289.6c-16.94-5.438-34.88 3.812-40.3 20.59C381.1 373.5 322.6 416 256 416c-37.96 0-73-14.18-100.2-37.8L200 334C216.9 317.2 205.2 288.6 181.6 288H34.66C24.32 288.3 16 296.7 16 307.1v145.9c0 24.04 29.07 36.08 46.07 19.07l48.5-48.53C149.5 458.8 200.6 480 255.1 480c94.45 0 177.4-60.34 206.4-150.2C467.9 313 458.6 294.1 441.8 289.6z"></path>
+                    </CardBtnSVG>
+                  </CardBtn>
+                </CardBtnlist>
+              ) : error ? (
+                <ErrorText>{repoErrorText(error)}</ErrorText>
+              ) : null}
               <CardLowerDetails>
                 Name: {card.name}
                 <br></br>
